22a31bccba235210aa8e34d96a399d910c450002
Trevor Burch <burchtm@rose-hulman.edu>
2015-11-11 02:32:27 -0500

    Issue #4 - Added checks and messages for empty fields on group and event creation

--- apps/SchedTek/common/js/controllers/GroupControllers/addgroup.js
+++ apps/SchedTek/common/js/controllers/GroupControllers/addgroup.js
@@ -1,9 +1,13 @@
 App.controller('groupAddController', ['$scope', 'SharedUserService', '$location', function($scope, SharedUserService, $location){
 	$scope.User = SharedUserService;
-	$scope.message = 'specific group controller stuffs';
 	
 	//adds group and the current user into DB
 	$scope.add = function(){
+		if($scope.gname==null || $scope.gdesc==null){
+			$scope.message = "Please Try Again";
+			return;
+		}
+		
 		console.log($('#name').val() + " " + $('#desc').val() + " " + $scope.User.userID);
 		var Data = {
 				adapter : 'SchedTekDB',
